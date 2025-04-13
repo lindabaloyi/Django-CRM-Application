3bab0151240db93a517419a56779cc8598ab7802
lindabaloyi <lindabaloyi10@gmail.com>
2025-04-13 18:52:19 +0200

    Update Django CRM app with latest code changes

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('my-login/', views.my_login, name='my-login'),
     path('dashboard/', views.dashboard, name="dashboard"),
+    path('user-logout/', views.user_logout, name="user-logout"),
+
+    #CRUD
+    path('create-record', views.create_record, name='create-record'),
+    path('update-record/<int:pk>', views.update_record, name='update-record'),
+    path('record/<int:pk>', views.singular_record, name='record'), 
+    path('delete-record/<int:pk>', views.delete_record, name='delete-record'),   
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm, LoginForm
+from .forms import CreateUserForm, LoginForm, CreateRecordForm, UpdateRecordForm
 from django.contrib import messages
-
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate
-
 from django.contrib.auth.decorators import login_required
-
-
+from .models import Record
 
 # - Homepage 
 
@@ -71,5 +68,76 @@ def my_login(request):
 
 @login_required(login_url='my-login')
 def dashboard(request):
+    
+    my_records = Record.objects.all()
+    context = {'records': my_records}
 
-    return render(request, 'webapp/dashboard.html')
+    return render(request, 'webapp/dashboard.html', context=context)
+
+
+# Create a recod
+@login_required(login_url='my-login')
+def create_record(request):
+    
+    form = CreateRecordForm()
+    
+    if request.method == "POST":
+        form = CreateRecordForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+    
+    context = {'form': form}
+    return render(request, 'webapp/create-record.html', context=context)
+
+    
+
+# - User logout
+
+def user_logout(request):
+
+    auth.logout(request)
+
+    messages.success(request, "Logout success!")
+
+    return redirect("my-login")
+
+# Update a record
+@login_required(login_url="my-login")
+def update_record(request, pk):
+    record = Record.objects.get(id=pk)
+
+    if request.method == 'POST':
+        form = UpdateRecordForm(request.POST, instance=record)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')  # fixed typo
+    else:
+        form = UpdateRecordForm(instance=record)
+
+    context = {'form': form}
+    return render(request, 'webapp/update-record.html', context=context)
+
+
+# Read / View a singular record
+@login_required(login_url='my-login')
+def singular_record(request, pk):
+
+    all_records = Record.objects.get(id=pk)
+
+    context = {'record':all_records}
+
+    return render(request, 'webapp/view-record.html', context=context)
+
+# - Delete a record
+
+@login_required(login_url='my-login')
+def delete_record(request, pk):
+
+    record = Record.objects.get(id=pk)
+
+    record.delete()
+
+    messages.success(request, "Your record was deleted!")
+
+    return redirect("dashboard")
